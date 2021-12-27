669a8bf527413289e6e9801c4384e64db0823574
Amirlan <amirlansd@gmail.com>
2021-12-27 17:58:52 +0600

    [Change] Change padding and delete fucntion of mutable list [Add] Add function of animation of dots

--- app/src/main/java/com/example/sauexpert/signup_patient/CutomKeyboardOTPScreen.kt
+++ app/src/main/java/com/example/sauexpert/signup_patient/CutomKeyboardOTPScreen.kt
@@ -2,7 +2,10 @@ package com.example.otp_example
 
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -11,7 +14,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,7 +21,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,127 +28,47 @@ import com.example.sauexpert.R
 
 @Composable
 fun CustomKeyboardOTPScreen() {
-    val inputVal = remember { mutableStateOf("") }
-    val scrollState = rememberScrollState()
     val pinList = remember { mutableStateListOf<String>() }
-
     val pinList2 = remember { mutableStateListOf<String>() }
-    for (i in pinList.indices) {
-        inputVal.value = pinList.get(i)
-    }
     val context = LocalContext.current
 
-
     CustomKeyboard(
-        input = inputVal.value,
-        scrollState = scrollState,
         list = pinList,
         list2 = pinList2,
         onClick = { digit ->
-            if (pinList.size < 4 && digit.toString() != "D") {
+            if (pinList.size < 4) {
                 pinList.add(digit.toString())
-//                inputVal.value += digit.toString()
-            } else if (pinList.size == 4 && pinList2.size < 4 && digit.toString() != "D") {
+            } else if (pinList.size == 4 && pinList2.size < 4) {
                 pinList2.add(digit.toString())
-            } else if (pinList2.size > 0 && digit.toString() == "D") {
-                pinList2.remove(pinList2.last())
-            } else if (pinList2.size == 0 && pinList.size > 0 && digit.toString() == "D") {
-                pinList.remove(pinList.last())
-//            } else if (pinList == pinList2) {
-//                Toast.makeText(context, "Text is equal", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
             }
-
-
         }
     )
 }
 
 @Composable
 fun CustomKeyboard(
-    input: String,
-    scrollState: ScrollState,
     list: MutableList<String>,
     list2: MutableList<String>,
     onClick: (digit: Char) -> Unit
 ) {
-    val rowWidth = 15.dp
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        TopBarForOTP(Modifier.padding(20.dp))
+        TopBarForOTP()
 
         PasscodeScreenDescription()
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            AnimatedVisibility(visible = list.size > 0) {
-                doteImage(modifier = Modifier)
-
-            }
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list.size > 1) {
-                doteImage(modifier = Modifier)
-
-            }
-
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list.size > 2) {
-                doteImage(modifier = Modifier)
-
-            }
-
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list.size > 3) {
-                doteImage(modifier = Modifier)
-
-            }
-        }
+        visibleDotsForPin(pinList = list)
 
         Spacer(modifier = Modifier.height(15.dp))
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            AnimatedVisibility(visible = list2.size > 0) {
-                doteImage(modifier = Modifier)
-
-            }
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list2.size > 1) {
-                doteImage(modifier = Modifier)
-
-            }
-
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list2.size > 2) {
-                doteImage(modifier = Modifier)
-
-            }
-
-            Spacer(modifier = Modifier.width(rowWidth))
-
-            AnimatedVisibility(visible = list2.size > 3) {
-                doteImage(modifier = Modifier)
-
-            }
-        }
+        visibleDotsForPin(pinList = list2)
 
         Spacer(modifier = Modifier.height(10.dp))
 
@@ -171,38 +92,11 @@ fun CustomKeyboard(
             Spacer(modifier = Modifier.width(20.dp))
 
             DeleteLeftIcon(
-                onClick = onClick
+                list = list,
+                list2 = list2
             )
         }
     }
-}
-
-@Composable
-fun TopBarForOTP(
-    modifier: Modifier = Modifier
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start,
-        modifier = modifier.fillMaxWidth()
-    ) {
-        Text(
-            text = stringResource(id = R.string.skip),
-            style = MaterialTheme.typography.body1
-        )
-    }
-}
-
-@Composable
-fun doteImage(
-    modifier: Modifier
-) {
-    Image(
-        painter = painterResource(id = R.drawable.ic_oval),
-        contentDescription = null,
-        modifier = modifier
-            .size(13.dp)
-    )
 }
 
 
@@ -269,17 +163,24 @@ fun PhoneButtonZero(
 
 @Composable
 fun DeleteLeftIcon(
-    onClick: (digit: Char) -> Unit,
+    list: MutableList<String>,
+    list2: MutableList<String>,
     modifier: Modifier = Modifier
 ) {
-    val number: String = "D"
+    val context = LocalContext.current
     Image(
         painter = painterResource(id = R.drawable.ic_delete_left),
         contentDescription = null,
         modifier = modifier
             .size(38.dp)
             .clickable {
-                onClick(number.first())
+                if (list2.size > 0) {
+                    list2.remove(list2.last())
+                } else if (list2.size == 0 && list.size > 0) {
+                    list.remove(list.last())
+                } else {
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+                }
             }
     )
 }
@@ -312,4 +213,74 @@ fun PasscodeScreenDescription(
             style = MaterialTheme.typography.subtitle1
         )
     }
+}
+
+@Composable
+fun TopBarForOTP(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = modifier.fillMaxWidth().padding(20.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.skip),
+            style = MaterialTheme.typography.body1,
+            modifier = modifier
+                .clickable {
+                }
+        )
+    }
+}
+
+
+@Composable
+fun visibleDotsForPin(
+    pinList: MutableList<String>
+) {
+    val rowWidth = 15.dp
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        AnimatedVisibility(visible = pinList.size > 0) {
+            doteImage(modifier = Modifier)
+
+        }
+        Spacer(modifier = Modifier.width(rowWidth))
+
+        AnimatedVisibility(visible = pinList.size > 1) {
+            doteImage(modifier = Modifier)
+
+        }
+
+        Spacer(modifier = Modifier.width(rowWidth))
+
+        AnimatedVisibility(visible = pinList.size > 2) {
+            doteImage(modifier = Modifier)
+
+        }
+
+        Spacer(modifier = Modifier.width(rowWidth))
+
+        AnimatedVisibility(visible = pinList.size > 3) {
+            doteImage(modifier = Modifier)
+
+        }
+    }
+
+}
+
+@Composable
+fun doteImage(
+    modifier: Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_oval),
+        contentDescription = null,
+        modifier = modifier
+            .size(13.dp)
+    )
 }
