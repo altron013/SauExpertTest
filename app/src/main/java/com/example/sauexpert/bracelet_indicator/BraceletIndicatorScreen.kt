47bc8c15464f73dc82909c6fcd162d015a89105f
Amirlan <amirlansd@gmail.com>
2022-01-13 15:29:56 +0600

    [Add] Added value for string and field for sp02 tab [Change] Change Bar Chart for resiving input from data class [In Progress] Line Chart for SP02

--- app/src/main/java/com/example/sauexpert/bracelet_indicator/BraceletIndicatorScreen.kt
+++ app/src/main/java/com/example/sauexpert/bracelet_indicator/BraceletIndicatorScreen.kt
@@ -1,11 +1,11 @@
 package com.example.sauexpert.bracelet_indicator
 
+import android.graphics.Paint
 import androidx.compose.animation.core.FloatTweenSpec
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import android.graphics.Paint
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sauexpert.R
+import com.example.sauexpert.model.SleepData
 import com.example.sauexpert.model.TextOfTabData
 import com.example.sauexpert.ui.theme.Gray30
 import com.example.sauexpert.widgets.compose.MainButton
@@ -86,7 +89,32 @@ fun BraceletIndicatorScreen() {
                 ) {
                     SleepStat()
                     Spacer(modifier = Modifier.height(12.dp))
-                    BarChart()
+                    BarChartForSleep(
+                        SleepData = listOf(
+                            SleepData(positionOnX = 10f, hourOfSleep = 140f, dateName = "Пн"),
+                            SleepData(positionOnX = 100f, hourOfSleep = 200f, dateName = "Вт"),
+                            SleepData(
+                                positionOnX = 190f,
+                                hourOfSleep = 190f,
+                                startTime = 40f,
+                                dateName = "Ср"
+                            ),
+                            SleepData(
+                                positionOnX = 280f,
+                                hourOfSleep = 180f,
+                                startTime = 60f,
+                                dateName = "Чт"
+                            ),
+                            SleepData(positionOnX = 370f, hourOfSleep = 220f, dateName = "Пт"),
+                            SleepData(
+                                positionOnX = 460f,
+                                hourOfSleep = 240f,
+                                startTime = 80f,
+                                dateName = "Сб"
+                            ),
+                            SleepData(positionOnX = 550f, hourOfSleep = 370f, dateName = "Вс")
+                        )
+                    )
                     Spacer(modifier = Modifier.height(275.dp))
                     SleepStat2()
                     Spacer(modifier = Modifier.height(16.dp))
@@ -106,6 +134,31 @@ fun BraceletIndicatorScreen() {
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                 )
+            }
+            1 -> Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 10.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth().verticalScroll(rememberScrollState())
+                        .padding(bottom = 60.dp)
+                ) {
+
+                    analysisSOASStat()
+                    Spacer(modifier = Modifier.height(24.dp))
+                    analysisSOASStat2()
+
+                }
+                MainButton(
+                    text = stringResource(id = R.string.range_customize),
+                    onClick = { /*TODO*/ },
+                    enableState = true,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                )
+
             }
 
         }
@@ -288,26 +341,11 @@ fun SleepStat(
 
 }
 
-data class Point(
-    val positionOnX: Float = 0f,
-    val sleepData: Float = 0f,
-    val startTime: Float = 0f,
-    val dateName: String = "Пн"
-)
-
 
 @Composable
-fun BarChart() {
-    val point = listOf(
-        Point(positionOnX = 10f, sleepData = 140f, dateName = "Пн"),
-        Point(positionOnX = 100f, sleepData = 200f, dateName = "Вт"),
-        Point(positionOnX = 190f, sleepData = 190f, startTime = 40f, dateName = "Ср"),
-        Point(positionOnX = 280f, sleepData = 180f, startTime = 60f, dateName = "Чт"),
-        Point(positionOnX = 370f, sleepData = 220f, dateName = "Пт"),
-        Point(positionOnX = 460f, sleepData = 240f, startTime = 80f, dateName = "Сб"),
-        Point(positionOnX = 550f, sleepData = 370f, dateName = "Вс")
-    )
-
+fun BarChartForSleep(
+    SleepData: List<SleepData>,
+) {
     var start by remember { mutableStateOf(false) }
     val heightPre by animateFloatAsState(
         targetValue = if (start) 1f else 0f,
@@ -446,11 +484,11 @@ fun BarChart() {
         )
 
         start = true
-        for (p in point) {
+        for (p in SleepData) {
             drawRect(
                 color = Color.Red,
                 topLeft = Offset(p.positionOnX + 20, p.startTime * heightPre),
-                size = Size(60f, p.sleepData * heightPre)
+                size = Size(60f, p.hourOfSleep * heightPre)
 
             )
 
@@ -568,6 +606,269 @@ fun ProgressBar(
             )
 
         }
+
+    }
+}
+
+
+@Composable
+fun SP02withLineGraph(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(7.dp)
+            )
+    ) {
+        SP02Stat()
+        Spacer(modifier = Modifier.height(12.dp))
+
+    }
+}
+
+@Composable
+fun SP02Stat(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(id = R.string.sp02),
+            style = MaterialTheme.typography.caption
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Circle,
+                contentDescription = "",
+                tint = Color.Green.copy(alpha = 0.25f),
+                modifier = modifier.size(9.dp)
+            )
+
+            Spacer(modifier = Modifier.width(2.dp))
+
+            Text(
+                text = stringResource(id = R.string.oxygen_level),
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Gray30
+            )
+        }
+
+        Text(
+            text = "18-20 ноября 2021",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold,
+            color = Gray30
+        )
+    }
+}
+
+data class Point(val X: Float = 0f, val Y: Float = 0f)
+
+@Composable
+fun lineChartForSp02() {
+    val point = listOf(
+        Point(10f, 10f), Point(50f, 100f), Point(100f, 30f),
+        Point(150f, 200f), Point(200f, 120f), Point(250f, 10f),
+        Point(300f, 280f), Point(350f, 100f), Point(400f, 10f),
+        Point(450f, 100f), Point(500f, 200f)
+    )
+    val path = Path()
+    for ((index, item) in point.withIndex()) {
+        if (index == 0) {
+            path.moveTo(item.X*scale, item.Y)
+        } else {
+            path.lineTo(item.X*scale, item.Y)
+        }
+    }
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .background(Color.White)
+            // Monitor gesture scaling
+            .graphicsLayer(
+            )
+    ) {
+        // draw  X Axis  Y Axis
+        drawLine(
+            start = Offset(10f, 300f),
+            end = Offset(10f, 0f),
+            color = Color.Black,
+            strokeWidth = 2f
+        )
+        drawLine(
+            start = Offset(10f, 300f),
+            end = Offset(510f, 300f),
+            color = Color.Black,
+            strokeWidth = 2f
+        )
+        // draw path
+        drawPath(
+            path = path,
+            color = Color.Blue,
+            style = Stroke(width = 2f)
+        )
+    }
+
+}
+
+
+@Composable
+fun analysisSOASStat(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.soas_analysis),
+                style = MaterialTheme.typography.subtitle2,
+            )
+
+            Text(
+                text = stringResource(R.string.more_detail),
+                style = MaterialTheme.typography.body2,
+                color = Color.Red
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(7.dp)
+                )
+        ) {
+            analysisStatField(
+                title = stringResource(R.string.severe_degree),
+                value = "18",
+                isIconVisible = true
+            )
+            Divider(
+                color = Gray30.copy(alpha = 0.19f),
+                thickness = 1.dp,
+                modifier = modifier
+                    .padding(horizontal = 16.dp)
+            )
+            analysisStatField(title = stringResource(R.string.sleep_apnea_case), value = "18")
+            Divider(
+                color = Gray30.copy(alpha = 0.19f),
+                thickness = 1.dp,
+                modifier = modifier
+                    .padding(horizontal = 16.dp)
+            )
+            analysisStatField(title = stringResource(R.string.hypopnea_case), value = "18")
+
+        }
+
+    }
+}
+
+@Composable
+fun analysisSOASStat2(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(
+            text = stringResource(R.string.soas_analysis),
+            style = MaterialTheme.typography.subtitle2,
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(7.dp)
+                )
+        ) {
+            analysisStatField(title = stringResource(R.string.sp02_average), value = "18")
+            Divider(
+                color = Gray30.copy(alpha = 0.19f),
+                thickness = 1.dp,
+                modifier = modifier
+                    .padding(horizontal = 16.dp)
+            )
+            analysisStatField(title = stringResource(R.string.breathing_rate), value = "18")
+            Divider(
+                color = Gray30.copy(alpha = 0.19f),
+                thickness = 1.dp,
+                modifier = modifier
+                    .padding(horizontal = 16.dp)
+            )
+            analysisStatField(title = stringResource(R.string.hypoxia_case), value = "18")
+            Divider(
+                color = Gray30.copy(alpha = 0.19f),
+                thickness = 1.dp,
+                modifier = modifier
+                    .padding(horizontal = 16.dp)
+            )
+            analysisStatField(title = stringResource(R.string.cardiac_pressure), value = "18")
+
+        }
+
+    }
+}
+
+
+@Composable
+fun analysisStatField(
+    title: String,
+    value: String,
+    isIconVisible: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(11.dp)
+    ) {
+
+        if (isIconVisible) {
+            Icon(
+                imageVector = Icons.Filled.Circle,
+                contentDescription = "",
+                tint = Color.Red,
+                modifier = modifier.size(9.dp)
+            )
+
+            Spacer(modifier = Modifier.width(2.dp))
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1,
+            )
+
+            Spacer(modifier = Modifier.width(75.dp))
+        } else {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1,
+            )
+        }
+
+
+        Text(
+            text = value,
+            style = MaterialTheme.typography.body1,
+        )
 
     }
 }
---
+++ app/src/main/java/com/example/sauexpert/model/SleepData.kt
@@ -0,0 +1,8 @@
+package com.example.sauexpert.model
+
+data class SleepData(
+    val positionOnX: Float = 0f,
+    val hourOfSleep: Float = 0f,
+    val startTime: Float = 0f,
+    val dateName: String = "Пн"
+)
