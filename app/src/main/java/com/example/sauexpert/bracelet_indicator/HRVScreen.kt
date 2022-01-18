624aa8f6383b51906b6c9749636ccaea9f96352a
Amirlan <amirlansd@gmail.com>
2022-01-18 16:48:41 +0600

    [Add] Added PopUp function for Bar Chart HRV Screen

--- app/src/main/java/com/example/sauexpert/bracelet_indicator/HRVScreen.kt
+++ app/src/main/java/com/example/sauexpert/bracelet_indicator/HRVScreen.kt
@@ -1,44 +1,38 @@
 package com.example.sauexpert.bracelet_indicator
 
 import android.graphics.Paint
-import android.widget.Toast
 import androidx.compose.animation.core.FloatTweenSpec
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FlashOn
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.Popup
 import com.example.sauexpert.R
 import com.example.sauexpert.model.HRVData
 import com.example.sauexpert.model.ListNumberOfYForTableData
 import com.example.sauexpert.ui.theme.Gray30
-import com.example.sauexpert.widgets.compose.MainButton
 
 
 @Composable
@@ -173,66 +167,40 @@ fun HRVStat(
 //    }
 //}
 
-private fun identifyClickItem(hrvData: List<HRVData>, x: Float): Int {
+private fun identifyClickItem(hrvData: List<HRVData>, x: Float, y: Float): Int {
     for ((index, hrvData) in hrvData.withIndex()) {
-        if (x > hrvData.positionOnX + 20 && x < hrvData.positionOnX + 80) {
+        if (x > hrvData.positionOnX + 20 && x < hrvData.positionOnX + 80
+            && y > hrvData.hourOfHRV + 20 && y < hrvData.hourOfHRV + 80
+        ) {
             return index
         }
     }
     return -1
 }
 
-//fun Modifier.customDialogPosition(x: Int, y: Int) = layout { measurable, constraints ->
-//    val placeable = measurable.measure(constraints)
-//    layout(constraints.maxWidth, constraints.maxHeight) {
-//        placeable.place(x - 400, y + 400, 10f)
-//    }
-//}
-
-
 @Composable
 fun BarChartForHRV(
     HRVData: List<HRVData>,
     ListNumberData: List<ListNumberOfYForTableData>
 ) {
-    val context = LocalContext.current
     var start by remember { mutableStateOf(false) }
     val heightPre by animateFloatAsState(
         targetValue = if (start) 1f else 0f,
         animationSpec = FloatTweenSpec(duration = 1000)
     )
 
-
-    val openDialog = remember { mutableStateOf(false) }
+    val visible = remember { mutableStateOf(false) }
     val itemID = remember { mutableStateOf(1) }
-//    val xPosition = remember { mutableStateOf(1) }
-//    val yPosition = remember { mutableStateOf(1) }
+    val positionOfX = remember { mutableStateOf(1) }
+    val positionOfY = remember { mutableStateOf(1) }
 
-
-    if (openDialog.value && itemID.value != -1) {
-        AlertDialog(
-            onDismissRequest = {
-                openDialog.value = false
-            },
-            title = {
-                Text(
-                    text = "${itemID.value} | ${HRVData[itemID.value].hourOfHRV} | ${HRVData[itemID.value].dateName}",
-                    style = MaterialTheme.typography.caption,
-                    modifier = Modifier.padding(16.dp)
-                )
-            },
-            buttons = {
-                MainButton(
-                    text = stringResource(id = R.string.understand),
-                    onClick = { openDialog.value = false },
-                    enableState = true,
-                    modifier = Modifier.padding(16.dp).size(70.dp)
-                )
-            },
-//            modifier = Modifier.customDialogPosition(xPosition.value, yPosition.value),
-            properties = DialogProperties()
-        )
-    }
+    InfoDialogForBarChartOfHRV(
+        visible = visible,
+        itemID = itemID,
+        xPosition = positionOfX,
+        yPosition = positionOfY,
+        HRVData = HRVData
+    )
 
     Canvas(
         modifier = Modifier
@@ -241,11 +209,10 @@ fun BarChartForHRV(
             .pointerInput(Unit) {
                 detectTapGestures(
                     onTap = {
-                        itemID.value = identifyClickItem(HRVData, it.x)
-                        Toast.makeText(context, "onTap: ${itemID.value}", Toast.LENGTH_SHORT).show()
-                        openDialog.value = true
-//                        xPosition.value = it.x.toInt()
-//                        yPosition.value = it.y.toInt()
+                        itemID.value = identifyClickItem(HRVData, it.x, it.y)
+                        visible.value = true
+                        positionOfX.value = it.x.toInt()
+                        positionOfY.value = it.y.toInt()
                     }
                 )
             }
@@ -286,7 +253,7 @@ fun BarChartForHRV(
         start = true
         for (p in HRVData) {
             drawRect(
-                color = Color.Red,
+                color = Color(250, 218, 221),
                 topLeft = Offset(
                     p.positionOnX + 20,
                     140.dp.toPx() - (140.dp.toPx() - p.hourOfHRV) * heightPre
@@ -304,6 +271,46 @@ fun BarChartForHRV(
                 160.dp.toPx(),
                 paint
             )
+        }
+    }
+}
+
+@Composable
+fun InfoDialogForBarChartOfHRV(
+    visible: MutableState<Boolean>,
+    itemID: MutableState<Int>,
+    xPosition: MutableState<Int>,
+    yPosition: MutableState<Int>,
+    HRVData: List<HRVData>,
+    modifier: Modifier = Modifier
+) {
+    if (visible.value) {
+        Box {
+            Popup(
+                alignment = Alignment.Center,
+                IntOffset(xPosition.value, yPosition.value - 70)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(width = 140.dp, height = 40.dp)
+                        .background(Color.White, RoundedCornerShape(10.dp))
+                ) {
+                    if (itemID.value == -1) {
+                        visible.value = false
+                    } else {
+                        Text(
+                            text = "${itemID.value} | ${HRVData[itemID.value].hourOfHRV} | " +
+                                    "${HRVData[itemID.value].dateName}",
+                            style = MaterialTheme.typography.h5,
+                            modifier = modifier
+                                .align(alignment = Alignment.Center)
+                                .clickable {
+                                    visible.value = false
+                                }
+                        )
+                    }
+                }
+            }
         }
     }
 }
