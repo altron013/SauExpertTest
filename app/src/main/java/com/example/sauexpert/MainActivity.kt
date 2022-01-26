017bdf4236566483621059f5da5463ed9edea419
Bekzat <bekzat.kambar@eca.kz>
2022-01-26 12:16:52 +0600

    [ADD] ic_plus_circle.xml conflict resolution

--- app/src/main/java/com/example/sauexpert/MainActivity.kt
+++ app/src/main/java/com/example/sauexpert/MainActivity.kt
@@ -10,12 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
