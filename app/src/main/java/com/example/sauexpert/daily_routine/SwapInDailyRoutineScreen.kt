e4f971bad829a378b98021fa5e5a48f9aca6cad4
Amirlan <amirlansd@gmail.com>
2022-02-04 11:22:33 +0600

    [Add] Added List For SwapInDailyRoutineScreen

--- app/src/main/java/com/example/sauexpert/daily_routine/SwapInDailyRoutineScreen.kt
+++ app/src/main/java/com/example/sauexpert/daily_routine/SwapInDailyRoutineScreen.kt
@@ -1,10 +1,20 @@
 package com.example.sauexpert.daily_routine
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.RemoveCircle
+import androidx.compose.material.icons.filled.Reorder
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.sauexpert.R
@@ -32,7 +42,7 @@ fun SwapDailyRoutineScreen() {
 
         Spacer(modifier = Modifier.height(23.dp))
 
-        MainSectionForAddNewMeal()
+        MainSectionForSwap()
 
     }
 }
@@ -47,5 +57,72 @@ fun MainSectionForSwap() {
         TimeActivity(activity = "Ужин", time = "10:00"),
     )
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(10.dp)
+            )
+    ) {
+        for (i in listActivity) {
+            ItemForList(
+                title = i.activity,
+                time = i.time
+            )
+            Spacer(modifier = Modifier.height(5.dp))
 
+        }
+    }
 }
+
+
+@Composable
+fun ItemForList(
+    title: String,
+    time: String,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth()
+            .padding(vertical = 11.dp, horizontal = 16.dp)
+    ) {
+
+        Icon(
+            imageVector = Icons.Default.RemoveCircle,
+            contentDescription = null,
+            tint = Color.Red,
+            modifier = modifier.size(18.dp).clickable { }
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.body1,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = time,
+            style = MaterialTheme.typography.body1,
+        )
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        Icon(
+            imageVector = Icons.Default.Reorder,
+            contentDescription = null,
+            tint = Color.Black,
+            modifier = modifier.size(18.dp).clickable {
+
+            }
+        )
+    }
+}
+
